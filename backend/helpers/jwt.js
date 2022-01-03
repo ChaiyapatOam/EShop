b1ba8eb1b79117d4aa4dbae3ee480ca91a4b2745
Chaiyapat Meeying <chaiyapatoam12@gmail.com>
2022-01-03 07:35:13 +0700

    User and Admin Role

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -4,7 +4,24 @@ function  authJwt() {
     const secret = process.env.secret
     return expressJwt({
         secret,
-        algorithms:['HS256']
+        algorithms:['HS256'],
+        isRevoked: isRevoked
+    }).unless({
+        path:[
+            {url: /\/api\/v1\/products(.*)/, methods:['GET','OPTIONS']},
+            {url: /\/api\/v1\/categories(.*)/, methods:['GET','OPTIONS']},
+            '/api/v1/users/login',
+            '/api/v1/users/resgister'
+        ]
     })
 }
+
+async function isRevoked(req,payload, done) {
+    if(!payload.isAdmin){
+        done(null, true)
+    }
+
+    done()
+}
+
 module.exports = authJwt
--- backend/routers/users.js
+++ backend/routers/users.js
@@ -46,7 +46,8 @@ router.post('/login', async (req,res) => {
     if(user &&  bcrypt.compareSync(req.body.password, user.passwordHash)) {
         const token = jwt.sign(
             {
-                userId: user.id
+                userId: user.id,
+                isAdmin: user.isAdmin
             },
             'secret'
         )
@@ -56,6 +57,29 @@ router.post('/login', async (req,res) => {
     } 
 })
 
+
+// Count User
+router.get('/get/count', async (req,res) => {
+    const userCount = await User.countDocuments()
+
+    if(!userCount){
+        res.status(500).json({success:false})
+    }
+    res.send({
+        userCount: userCount})
+})
+//Delete User
+router.delete('/:id',(req,res) => {
+    User.findByIdAndRemove(req.params.id).then(user =>{
+        if(user) {
+            return res.status(200).json({success:true,message:"The user is deleted"})
+        } else {
+            return res.status(404).json({success:false,message:"user not found"})
+        }
+    }).catch((err)=>{
+        return res.status(400).json({success:false,err: err})
+    })
+})
 
 
 module.exports = router
